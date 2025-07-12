da3250584366cf5a760d81cb6e351cec738bcefc
ItsOfficiallyMe <mr.aiofkingdom@gmail.com>
2025-07-12 11:49:51 +0530

    Update demographic_data_analyzer.py

--- demographic_data_analyzer.py
+++ demographic_data_analyzer.py
@@ -1,1 +1,60 @@
+import pandas as pd
 
+def calculate_demographic_data(print_data=True):
+    df = pd.read_csv('adult.data.csv')
+
+    race_count = df['race'].value_counts()
+
+    average_age_men = round(df[df['sex'] == 'Male']['age'].mean(), 1)
+
+    percentage_bachelors = round(
+        (df['education'] == 'Bachelors').sum() * 100 / len(df), 1)
+
+    higher_edu = df['education'].isin(['Bachelors', 'Masters', 'Doctorate'])
+    lower_edu = ~higher_edu
+
+    higher_edu_rich = round(
+        (df[higher_edu]['salary'] == '>50K').sum() * 100 / higher_edu.sum(), 1)
+
+    lower_edu_rich = round(
+        (df[lower_edu]['salary'] == '>50K').sum() * 100 / lower_edu.sum(), 1)
+
+    min_work_hours = df['hours-per-week'].min()
+
+    num_min_workers = df[df['hours-per-week'] == min_work_hours]
+    rich_percentage = round(
+        (num_min_workers['salary'] == '>50K').sum() * 100 / len(num_min_workers), 1)
+
+    country_salary = df[df['salary'] == '>50K']['native-country'].value_counts() * 100 / \
+        df['native-country'].value_counts()
+    highest_earning_country = country_salary.idxmax()
+    highest_earning_country_percentage = round(country_salary.max(), 1)
+
+    india_occupations = df[(df['native-country'] == 'India') &
+                           (df['salary'] == '>50K')]
+    top_IN_occupation = india_occupations['occupation'].value_counts().idxmax()
+
+    if print_data:
+        print("Number of each race:\n", race_count)
+        print("Average age of men:", average_age_men)
+        print(f"Percentage with Bachelors degrees: {percentage_bachelors}%")
+        print(f"Percentage with higher education that earn >50K: {higher_edu_rich}%")
+        print(f"Percentage without higher education that earn >50K: {lower_edu_rich}%")
+        print(f"Min work time: {min_work_hours} hours/week")
+        print(f"Percentage of rich among those who work fewest hours: {rich_percentage}%")
+        print("Country with highest percentage of rich:", highest_earning_country)
+        print("Highest percentage of rich people in country:", highest_earning_country_percentage)
+        print("Top occupations in India for those who earn >50K:", top_IN_occupation)
+
+    return {
+        'race_count': race_count,
+        'average_age_men': average_age_men,
+        'percentage_bachelors': percentage_bachelors,
+        'higher_education_rich': higher_edu_rich,
+        'lower_education_rich': lower_edu_rich,
+        'min_work_hours': min_work_hours,
+        'rich_percentage': rich_percentage,
+        'highest_earning_country': highest_earning_country,
+        'highest_earning_country_percentage': highest_earning_country_percentage,
+        'top_IN_occupation': top_IN_occupation
+  }
